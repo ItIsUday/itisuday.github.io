9eb08e52330c54d2b38f34c220b2a6214093de0a
ItIsUday <57130884+ItIsUday@users.noreply.github.com>
2026-06-06 15:12:02 +0000

    Deploying to gh-pages from @ ItIsUday/itisuday.github.io@4c80d498c55a0374039d40d094976e4cbfb1558e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,7 +16,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-wu-wei-conscious-and-the-unconscious",
+        },{id: "post-anyone-dread-the-selfless-i",
+        
+          title: "Anyone Dread: The Selfless I",
+        
+        description: "The randomness of being born",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/anyone-dread/";
+          
+        },
+      },{id: "post-wu-wei-conscious-and-the-unconscious",
         
           title: "Wu Wei: Conscious and the Unconscious",
         
@@ -26,52 +37,6 @@ ninja.data = [{
           
             window.location.href = "/blog/2026/wu-wei/";
           
-        },
-      },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
